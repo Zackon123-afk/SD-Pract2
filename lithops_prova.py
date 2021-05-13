e12a7ab6b2fff6eee3ac4d7bb1159496e1bfad38
alexsori12 <asorianofa@gmail.com>
2021-05-13 18:48:13 +0200

    avançant

--- lithops_prova.py
+++ lithops_prova.py
@@ -1,5 +1,9 @@
-from lithops import Storage
+#from lithops import Storage
+from lithops.multiprocessing import Pool
 
-storage = Storage()
+def double(i):
+    return i * 2
 
-storage.put_object('2sdpract','test.txt','Hello World')
+with Pool() as pool:
+    result = pool.map(double, [1, 2, 3, 4, 5])
+    print(result)
--- twitter_analize.py
+++ twitter_analize.py
@@ -1,12 +1,14 @@
 import tweepy
 import pandas as pd
+from lithops import Storage
 
 auth = tweepy.OAuthHandler("G7oVPMZP776iDbfLW6KRIlvg6", "MnH3qXuRHfoJXSzXSPdtneAvLCJ2MslvKskHHq0qvrAdNiUyox")
 auth.set_access_token("1059931089999945729-AAimzlFRpPy6RSQqESCM1XJJZAmtbn", "4Sq5Ga0aLC2PIgdzWWIp5ISY4iNFg6cRshJJpQUv12j9u")
 
 api = tweepy.API(auth)
 
-numberOfTweets=20
+numberOfTweets=5
+
 cursor = tweepy.Cursor(api.search, q="covid-19 lang=ca OR lang:es",tweet_mode="extended").items(numberOfTweets)
 tweets=[]
 likes=[]
@@ -17,12 +19,17 @@ for i in cursor:
     tweets.append(i.full_text)
     likes.append(i.favorite_count)
     time.append(i.created_at)
-    comments.append(i.comment_count)
 
-df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time,'comments':comments})
+
+df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time})
 df = df[~df.tweets.str.contains("RT")]
 df = df.reset_index(drop=True)
 df
+
+nom_bucket="2sdpractica"
+
+storage = Storage()
+storage.put_object(nom_bucket,"twitter_analize.json",df.to_json(orient="index"))
 
 #public_tweets = api.home_timeline()
 #for tweet in public_tweets:
