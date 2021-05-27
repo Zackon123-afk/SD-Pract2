c64e8333c889e58d783537e08fcce768a921668e
alexsori12 <asorianofa@gmail.com>
2021-05-27 12:16:24 +0200

    Docker probado y funcional

--- lithops_prova.py
+++ lithops_prova.py
@@ -1,9 +1,11 @@
 from lithops import Storage
 from lithops.multiprocessing import Pool
+import tweepy
+
 
 storage = Storage()
 
-storage.put_object('2sdpractica','test.txt','ARNAU no FOLLA')
+storage.put_object('2sdpractica','test.txt','jajaj xd')
 
 def double(i):
     return i * 2
@@ -11,5 +13,6 @@ def double(i):
 with Pool() as pool:
     result = pool.map(double, [1, 2, 3, 4, 5])
     print(result)
+
 
 
--- twitter_analize_push.py
+++ twitter_analize_push.py
@@ -1,22 +1,32 @@
-import tweepy
 from datetime import datetime
 from lithops import Storage
+from lithops.multiprocessing import Pool
 from autenticacion_tweppy import get_auth
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
+import tweepy
 
 
+def get_auth():
+    auth = tweepy.OAuthHandler("G7oVPMZP776iDbfLW6KRIlvg6", "MnH3qXuRHfoJXSzXSPdtneAvLCJ2MslvKskHHq0qvrAdNiUyox")
+    auth.set_access_token("1059931089999945729-AAimzlFRpPy6RSQqESCM1XJJZAmtbn", "4Sq5Ga0aLC2PIgdzWWIp5ISY4iNFg6cRshJJpQUv12j9u")
+    return auth
 
 def tweepy_scan(word, nom_bucket):
+    
+
+    storage = Storage()
+    storage.put_object('2sdpractica','testxd.txt','jajajjaa')
     
     auth = get_auth()
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
+   
     #create_headercsv(filename)
    
     analyzer = SentimentIntensityAnalyzer()
     qstring=word+" lang=ca OR lang:es"
-   
+
     
     i = 0;
     textos = []
@@ -40,8 +50,6 @@ def tweepy_scan(word, nom_bucket):
         lenguaje.append(str(status.lang))
         i += 1;
 
-       
-    
     datos = {
         "Mensaje": textos,
         "url": urls,
@@ -58,9 +66,19 @@ def tweepy_scan(word, nom_bucket):
     storage = Storage()  
     storage.put_object(nom_bucket,data+"-"+word+".json",json.dumps(datos))
 
-#----------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 if __name__ == '__main__':
-    tweepy_scan("covid","2sdpractica")    
+ 
+    
+   
+    
+    with Pool() as pool:
+        result = pool.starmap(tweepy_scan, [("covid","2sdpractica"),("moderna","2sdpractica")] )
+    
+       
+
+
+#("pfizer","2sdpractica"), ("moderna","2sdpractica"), ("astrazeneca","2sdpractica"), ("sputnik v","2sdpractica"), ("janssen","2sdpractica")])
 
