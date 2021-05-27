eb85bdaee0e2ae7aa28c71159f09f2f2a87f8668
alexsori12 <asorianofa@gmail.com>
2021-05-27 18:14:19 +0200

    Twitter done
    Se puede mejorar el aspecto de las graficas y el tamaño de los datos

--- autenticacion_tweppy.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-
-'''
-Función utilizada para utilizar la Api de Twitter
-'''
-def get_auth():
-    auth = tweepy.OAuthHandler("G7oVPMZP776iDbfLW6KRIlvg6", "MnH3qXuRHfoJXSzXSPdtneAvLCJ2MslvKskHHq0qvrAdNiUyox")
-    auth.set_access_token("1059931089999945729-AAimzlFRpPy6RSQqESCM1XJJZAmtbn", "4Sq5Ga0aLC2PIgdzWWIp5ISY4iNFg6cRshJJpQUv12j9u")
-    return auth
--- twitter_analize_push.py
+++ twitter_analize_push.py
@@ -1,33 +1,30 @@
 from datetime import datetime
 from lithops import Storage
 from lithops.multiprocessing import Pool
-from autenticacion_tweppy import get_auth
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
 import tweepy
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import collections
+
+BUCKET="2sdpractica"
 
 def get_auth():
     auth = tweepy.OAuthHandler("G7oVPMZP776iDbfLW6KRIlvg6", "MnH3qXuRHfoJXSzXSPdtneAvLCJ2MslvKskHHq0qvrAdNiUyox")
     auth.set_access_token("1059931089999945729-AAimzlFRpPy6RSQqESCM1XJJZAmtbn", "4Sq5Ga0aLC2PIgdzWWIp5ISY4iNFg6cRshJJpQUv12j9u")
     return auth
 
-def tweepy_scan(word, nom_bucket):
-    
+def tweepy_scan(word):
+    global BUCKET
 
-    storage = Storage()
-    storage.put_object('2sdpractica','testxd.txt','jajajjaa')
-    
     auth = get_auth()
     api = tweepy.API(auth, wait_on_rate_limit=True)
-
-   
-    #create_headercsv(filename)
    
     analyzer = SentimentIntensityAnalyzer()
     qstring=word+" lang=ca OR lang:es"
 
-    
     i = 0;
     textos = []
     urls = []
@@ -39,8 +36,7 @@ def tweepy_scan(word, nom_bucket):
     
     for status in tweepy.Cursor(api.search, q=qstring ,tweet_mode="extended").items(500): #numberOftwets
 
-        print("-------------------------------------------------------------------------------------------------------------------------------" + str(i))
-
+        #print("-------------------------------------------------------------------------------------------------------------------------------" + str(i))
         textos.append(status.full_text)
         urls.append("https://twitter.com/twitter/statuses/"+str(status.id)+",")
         sentiments.append(str(analyzer.polarity_scores(textos[i])['compound']))
@@ -59,26 +55,69 @@ def tweepy_scan(word, nom_bucket):
         "source": source,
         "lenguaje": lenguaje
     }
+    
     now = datetime.now()
     data = now.strftime("%m/%d/%Y")
 
-    
     storage = Storage()  
-    storage.put_object(nom_bucket,data+"-"+word+".json",json.dumps(datos))
+    storage.put_object(BUCKET,data+"-"+word+".json",json.dumps(datos))
 
+def grafic_twitter(word):
+    global BUCKET
+    
+    print("wtf")
+    now = datetime.now()
+    data = now.strftime("%m/%d/%Y")
+    key= data +"-"+ word +'.json'
+
+    storage = Storage()
+    json_read = storage.get_object(BUCKET,key)
+    data_analize = json.loads(json_read)
+    df = pd.DataFrame.from_dict(data_analize,orient='columns')
+    df["sentiment"]=df["sentiment"].astype(float)
+
+    sent_pos=len(df[df["sentiment"] >= 0])
+    sent_neg=len(df[df["sentiment"] < 0])
+    sents=[sent_pos,sent_neg]
+    noms=['Positiu','Negatiu']
+   
+    ###############################################################################
+    
+    llista=list(df["sentiment"].astype(float))
+    ocurrences = collections.Counter(llista)
+    dictionary = ocurrences.items()
+    dfa= pd.DataFrame.from_dict(dictionary,orient='columns')
+    dfa.columns = ['value','quantity']
+    dfa['quantity']= dfa['quantity'].astype(float)
+    x_value=list(dfa["value"])
+    y_value=list(dfa["quantity"])
+
+    fig, (axs1, axs2)= plt.subplots(1,2)
+
+    axs1.bar(noms,sents)
+    axs1.set_title('Diferencia del sentiment')
+    axs2.bar(x_value,y_value)
+    axs2.set_title('Concentració del sentiment')
+    fig.suptitle('Estudi de la paraula:'+word)
+    # plt.bar(x_value,y_value)
 
 
 
 if __name__ == '__main__':
  
+    #with Pool() as pool:
+        #pool.map(tweepy_scan,  [ "covid", "moderna"])
+        #pool.map(tweepy_scan,  [ "pfizer", "astrazeneca"])
+        #pool.map(tweepy_scan,  [ "sputnik v", "janssen"])
+        #pool.map(grafic_twitter, [ "covid", "moderna"])#, "pfizer", "astrazeneca", "sputnik v","janssen"])
     
-   
+
+    grafic_twitter("covid")
+    grafic_twitter("moderna")
+    grafic_twitter("pfizer")
+    grafic_twitter("astrazeneca")
+    grafic_twitter("sputnik v")
+    grafic_twitter("janssen")
     
-    with Pool() as pool:
-        result = pool.starmap(tweepy_scan, [("covid","2sdpractica"),("moderna","2sdpractica")] )
+
     
-       
-
-
-#("pfizer","2sdpractica"), ("moderna","2sdpractica"), ("astrazeneca","2sdpractica"), ("sputnik v","2sdpractica"), ("janssen","2sdpractica")])
-
