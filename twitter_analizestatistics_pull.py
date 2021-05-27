0109243826abef076acdc96834a5355a52766b6e
Zackon123-afk <arnaullaberia27@gmail.com>
2021-05-27 15:19:26 +0200

    analisi twitter casi fet

--- twitter_analizestatistics_pull.py
+++ twitter_analizestatistics_pull.py
@@ -1,32 +1,48 @@
+from re import A
 from lithops import Storage
 from datetime import datetime
 import pandas as pd
 from matplotlib import pyplot as plt
 import json
-import csv
+import numpy as np
+import collections
 
-words_to_read = {
-    'covid',
-    'moderna',
-    'pfizer',
-    'astrazeneca',
-    'vacuna'
-}
 nom_bucket="2sdpractica"
-now = datetime.now()
-data = now.strftime("%m/%d/%Y")
-word = 'covid'
-key= '05/17/2021 21:26:11-covid.json'
+key= '05/26/2021-covid.json'
 
 storage = Storage()
 json_read = storage.get_object(nom_bucket,key)
 data_analize = json.loads(json_read)
+df = pd.DataFrame.from_dict(data_analize,orient='columns')
+df["sentiment"]=df["sentiment"].astype(float)
 
-# pd.DataFrame(dict([ (k, pd.Series(v)) for k,v in data_analize.items() ]))
 
 
-print(data_analize['url'][1])
+sent_pos=len(df[df["sentiment"] >= 0])
+sent_neg=len(df[df["sentiment"] < 0])
+sents=[sent_pos,sent_neg]
+noms=['Positiu','Negatiu']
 
-# def countingWords(i):
-#     for word in words_to_read:
-#         data_analize['Mensaje'][i].contains(word)
+
+
+###############################################################################
+
+
+llista=list(df["sentiment"].astype(float))
+ocurrences = collections.Counter(llista)
+dictionary = ocurrences.items()
+dfa= pd.DataFrame.from_dict(dictionary,orient='columns')
+dfa.columns = ['value','quantity']
+dfa['quantity']= dfa['quantity'].astype(float)
+x_value=list(dfa["value"])
+y_value=list(dfa["quantity"])
+
+fig, (axs1, axs2)= plt.subplots(1,2)
+
+axs1.bar(noms,sents)
+axs1.set_title('Diferencia del sentiment')
+axs2.bar(x_value,y_value)
+axs2.set_title('Concentració del sentiment')
+fig.suptitle('Estudi de la paraula')
+# plt.bar(x_value,y_value)
+
