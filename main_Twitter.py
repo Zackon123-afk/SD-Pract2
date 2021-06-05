356a71f1fff2af8164def1f7696999e8de209891
Zackon123-afk <64971251+Zackon123-afk@users.noreply.github.com>
2021-06-05 12:21:47 +0200

    Update main_Twitter.py

--- main_Twitter.py
+++ main_Twitter.py
@@ -13,8 +13,8 @@ import collections
 BUCKET="2sdpractica"
 
 def get_auth():
-    auth = tweepy.OAuthHandler("G7oVPMZP776iDbfLW6KRIlvg6", "MnH3qXuRHfoJXSzXSPdtneAvLCJ2MslvKskHHq0qvrAdNiUyox")
-    auth.set_access_token("1059931089999945729-AAimzlFRpPy6RSQqESCM1XJJZAmtbn", "4Sq5Ga0aLC2PIgdzWWIp5ISY4iNFg6cRshJJpQUv12j9u")
+    auth = tweepy.OAuthHandler("", "")
+    auth.set_access_token("", "")
     return auth
 
 def tweepy_scan(word):
